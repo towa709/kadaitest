2496d62025cc8685e299254ea25ed5afb28542e3
towa <hoshi.no.natu@gmail.com>
2025-07-13 01:31:26 +0900

    Git管理からMySQLデータを除外

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -4,28 +4,27 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\App;
 
 class AuthServiceProvider extends ServiceProvider
 {
-  /**
-   * The policy mappings for the application.
-   *
-   * @var array<class-string, class-string>
-   */
-  protected $policies = [
-    // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-  ];
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
 
-  /**
-   * Register any authentication / authorization services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    $this->registerPolicies();
-    App::setLocale('ja');
-    //
-  }
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
 }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -6,16 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-  /**
-   * Seed the application's database.
-   *
-   * @return void
-   */
-  public function run()
-  {
-    $this->call([
-      CategoriesTableSeeder::class,
-      ContactSeeder::class, 
-    ]);
-  }
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+    }
 }
